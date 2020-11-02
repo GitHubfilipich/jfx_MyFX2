7d0118cffbc9e226f0f2b5ebbc67a8b2643f4112
fil <fil@fil.fil>
2020-11-02 22:31:12 +0200

    add filechooser

--- src/application/SampleController.java
+++ src/application/SampleController.java
@@ -2,6 +2,7 @@ package application;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
 import javafx.scene.control.Button;
 
 
@@ -13,6 +14,9 @@ public class SampleController {
 	
 	@FXML public void btn1Click(ActionEvent event) {
 		textField1.setText("Привет!");
+		FileChooser fil_chooser = new FileChooser();
+		fil_chooser.showOpenDialog(null);
+		textField1.setText("File chosen :)");
 	}
 	
 }
